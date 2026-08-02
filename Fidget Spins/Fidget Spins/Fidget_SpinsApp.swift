a1a220df7d6e067c0e71dab91454842b2060e6e3
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:18:28 -0400

    Add Fidget Spins/Fidget Spins/Fidget_SpinsApp.swift

---
+++ Fidget Spins/Fidget Spins/Fidget_SpinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fidget_SpinsApp.swift
+//  Fidget Spins
+//
+//  Created by Elliot Williams on 2025-06-22.
+//
+
+import SwiftUI
+
+@main
+struct Fidget_SpinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
