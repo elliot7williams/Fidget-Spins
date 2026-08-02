b90b2944469be24bb23f58d8cbdf6048fc8accfe
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:18:34 -0400

    Add Fidget Spins/Fidget SpinsTests/Fidget_SpinsTests.swift

---
+++ Fidget Spins/Fidget SpinsTests/Fidget_SpinsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fidget_SpinsTests.swift
+//  Fidget SpinsTests
+//
+//  Created by Elliot Williams on 2025-06-22.
+//
+
+import Testing
+@testable import Fidget_Spins
+
+struct Fidget_SpinsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
